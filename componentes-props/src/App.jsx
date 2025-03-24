b2e2f156100ea3f0f9b49c308e4603fe0fcabadb
mvserikawa <mvserikawa@gmail.com>
2025-03-24 13:44:45 -0300

    learn{ap04-react}: encerra a sessao 2.5
    -vamos comecar a refatoracao criando um componente que representa pedidos

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -2,13 +2,87 @@ const App = () => {
   return (
     <div className="container border rounded mt-2">
       <div className="row">
-        <div>a</div>
-        <div>b</div>
-    </div>
-    <div className="row"> 
-        <div>c</div>
+
+        <div className="col-12 col-md-6 col-xxl-3">
+
+          <div className="card">
+            <div className="card-header text muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-hippo fa-2x fa-beat"></i>
+              </div>
+            <div className="border flex-grow-1 ms-3 py-2">
+              <h4 className="text-center">Hipopótamo</h4>
+              <p className="text-center">É um hipopótamo fêmea</p>
+            </div>
+          </div>
+        </div>
+        
       </div>
+      
+      </div>
+    
+      <div className="col-12 col-md-6 col-xxl-3">
+      <div className="card">
+            <div className="card-header text muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-headset fa-2x fa-beat"></i>
+              </div>
+            <div className="border flex-grow-1 ms-3 py-2">
+              <h4 className="text-center">Headset</h4>
+              <p className="text-center">Headset Bluetooth</p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+      <div className="col-12 col-md-6 col-xxl-3">
+      <div className="card">
+            <div className="card-header text muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-gamepad fa-2x fa-beat"></i>
+              </div>
+            <div className="border flex-grow-1 ms-3 py-2">
+              <h4 className="text-center">Gamepad</h4>
+              <p className="text-center">Controle Nintendo</p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    
+
+      <div className="col-12 col-md-6 col-xxl-3">
+      <div className="card">
+            <div className="card-header text muted">
+              22/04/2025
+            </div>
+            <div className="card-body d-flex">
+              <div className="d-flex align-items-center">
+                <i className="fa-solid fa-snowman fa-2x fa-shake"></i>
+              </div>
+            <div className="border flex-grow-1 ms-3 py-2">
+              <h4 className="text-center">Boneco de Neve</h4>
+              <p className="text-center">Esse boneco de neve fica balançando sozinho</p>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+
     </div>
+    
 
   )
 
