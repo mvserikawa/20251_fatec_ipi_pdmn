febd893689794f1ca3ce76981411d841f68481c0
mvserikawa <mvserikawa@gmail.com>
2025-03-24 00:39:55 -0300

    learn{ap04-react} encerra o codigo 2.5.4
    - fizemos a responsividade para quatro caixinhas

---
+++ componentes-props/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="container border rounded mt-2">
+      <div className="row">
+        <div>a</div>
+        <div>b</div>
+    </div>
+    <div className="row"> 
+        <div>c</div>
+      </div>
+    </div>
+
+  )
+
+}
+export default App
