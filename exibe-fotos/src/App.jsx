68f739fadb15d46682f56bcf7c399bcab8f75619
mvserikawa <mvserikawa@gmail.com>
2025-05-16 22:07:53 -0300

    {ap08-learn-react}: faltou as ultimas alteracoes

--- exibe-fotos/src/App.jsx
+++ exibe-fotos/src/App.jsx
@@ -26,7 +26,7 @@ class App extends React.Component{
     pexelsClient.get('/search', {
       params: {
         query: termo,
-        per_page: 5
+        per_page: 15
       }
     }).then(result => this.setState({photos: result.data.photos}))
   }
@@ -57,7 +57,7 @@ class App extends React.Component{
           />
         </div>
         <div className="col-12">
-          <ListaImagens photos={this.state.photos}/>
+          <ListaImagens imgStyle={"col-12 md:col-6 lg:col-4 xl:col-3"} photos={this.state.photos}/>
         </div>
       </div>
     )
--- exibe-fotos/src/components/Imagem.jsx
+++ exibe-fotos/src/components/Imagem.jsx
@@ -1,12 +1,12 @@
 // rafce
 import React from 'react'
 
-const Imagem = ({src, alt}) => {
+const Imagem = ({src, alt, imgStyle}) => {
     //const {src, alt} = props //desestruturação do objeto
     // const src = props.src
     // const alt = props.alt
   return (
-    <div>
+    <div className={`${imgStyle} flex justiy-content-center`}>
         <img src={src} alt={alt}></img>
     </div>
   )
