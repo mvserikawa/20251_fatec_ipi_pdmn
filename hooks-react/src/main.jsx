9884da2299d434a76cba5411fb2e896f2e75009b
mvserikawa <mvserikawa@gmail.com>
2025-05-16 22:04:29 -0300

    learn{ap08-react}: encerra a sessao 2.10
    - retomar decidindo qual icone exibir e de quais contrair o conteudo

---
+++ hooks-react/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import 'primeflex/primeflex.min.css' 
+import 'primereact/resources/primereact.min.css' 
+import 'primereact/resources/themes/fluent-light/theme.css'
+import 'primeicons/primeicons.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
